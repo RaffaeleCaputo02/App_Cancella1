a4d49e40d15288eafb76d6a2046a28e3b5025003
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-07 09:46:43 +0100

    Es finito

--- App_Cancella/MainWindow.xaml.cs
+++ App_Cancella/MainWindow.xaml.cs
@@ -21,46 +21,64 @@ namespace App_Cancella
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool stop = false;
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         private void Btn_Start1_Click(object sender, RoutedEventArgs e)
         {
-            Task.Factory.StartNew(()=>DoWork());
+            if (stop) stop = false;
+            Task.Factory.StartNew(()=>DoWork(100,1000,lbl_Count1));
         }
 
-        private void DoWork()
+        private void DoWork(int max, int delay, Label lbl)
         {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i <= max; i++)
             {
-                Dispatcher.Invoke(()=>UpdateUI(i));
-                Thread.Sleep(1000);
+                Dispatcher.Invoke(()=>UpdateUI(i,lbl));
+                Thread.Sleep(delay);
+
+                if (stop) break;
+                
             }
         }
 
-        private void UpdateUI(int i)
+        private void UpdateUI(int i,Label lbl)
         {
-            lbl_Count1.Content = i.ToString();
+            lbl.Content = i.ToString();
         }
 
         private void Btn_Start2_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 100; i++)
-            {
-                Dispatcher.Invoke(() => UpdateUI(i));
-                Thread.Sleep(1000);
-            }
+            if (stop) stop = false;
+            int max = Convert.ToInt32(txt_Max1.Text);
+            Task.Factory.StartNew(() => DoWork(max, 1000,lbl_Count2));
         }
 
         private void Btn_Start3_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 100; i++)
-            {
-                Dispatcher.Invoke(() => UpdateUI(i));
-                Thread.Sleep(1000);
-            }
+            if (stop) stop = false;
+            int max = Convert.ToInt32(txt_Max2.Text);
+            int delay = Convert.ToInt32(txt_Delay.Text);
+            Task.Factory.StartNew(() => DoWork(max, delay,lbl_Count3));
+        }
+
+        private void Btn_Stop1_Click(object sender, RoutedEventArgs e)
+        {
+            stop = true;
+        }
+
+        private void Btn_Stop2_Click(object sender, RoutedEventArgs e)
+        {
+            stop = true;
+        }
+
+        private void Btn_Stop3_Click(object sender, RoutedEventArgs e)
+        {
+            stop = true;
         }
     }
 }
