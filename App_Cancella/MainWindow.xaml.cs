4ffb58b5b4397317910082d2f471d8a4e7338be4
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-21 09:53:31 +0100

    es finito

--- App_Cancella/MainWindow.xaml.cs
+++ App_Cancella/MainWindow.xaml.cs
@@ -21,7 +21,11 @@ namespace App_Cancella
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool stop = false;
+        //bool stop = false;
+        CancellationTokenSource ct1;
+        CancellationTokenSource ct2;
+        CancellationTokenSource ct3;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,18 +34,25 @@ namespace App_Cancella
 
         private void Btn_Start1_Click(object sender, RoutedEventArgs e)
         {
-            if (stop) stop = false;
-            Task.Factory.StartNew(()=>DoWork(100,1000,lbl_Count1));
+            ct1 = new CancellationTokenSource();
+            if (ct1.Token.IsCancellationRequested)
+            {
+                ct1.Cancel();
+            }
+            
+            //if (stop) stop = false;
+            Task.Factory.StartNew(()=>DoWork(100,1000,lbl_Count1,ct1));
         }
 
-        private void DoWork(int max, int delay, Label lbl)
+        private void DoWork(int max, int delay, Label lbl,CancellationTokenSource ct)
         {
+            
             for (int i = 0; i <= max; i++)
             {
                 Dispatcher.Invoke(()=>UpdateUI(i,lbl));
                 Thread.Sleep(delay);
 
-                if (stop) break;
+                if (ct.Token.IsCancellationRequested) break;//gracefully
                 Console.ReadLine();
                 
             }
@@ -54,32 +65,76 @@ namespace App_Cancella
 
         private void Btn_Start2_Click(object sender, RoutedEventArgs e)
         {
-            if (stop) stop = false;
+            ct2 = new CancellationTokenSource();
+            if (ct2.Token.IsCancellationRequested)
+            {
+                ct2.Cancel();
+            }
+            
+            //if (stop) stop = false;
             int max = Convert.ToInt32(txt_Max1.Text);
-            Task.Factory.StartNew(() => DoWork(max, 1000,lbl_Count2));
+            Task.Factory.StartNew(() => DoWork(max, 1000,lbl_Count2,ct2));
         }
 
         private void Btn_Start3_Click(object sender, RoutedEventArgs e)
         {
-            if (stop) stop = false;
+            ct3 = new CancellationTokenSource();
+            if (ct3.Token.IsCancellationRequested)
+            {
+                ct3.Cancel();
+            }
+            
+            //if (stop) stop = false;
             int max = Convert.ToInt32(txt_Max2.Text);
             int delay = Convert.ToInt32(txt_Delay.Text);
-            Task.Factory.StartNew(() => DoWork(max, delay,lbl_Count3));
+            Task.Factory.StartNew(() => DoWork(max, delay,lbl_Count3,ct3));
         }
 
         private void Btn_Stop1_Click(object sender, RoutedEventArgs e)
         {
-            stop = true;
+            if (ct1 != null)
+            {
+                ct1.Cancel();
+            }
+            //stop = true;
         }
 
         private void Btn_Stop2_Click(object sender, RoutedEventArgs e)
         {
-            stop = true;
+            if (ct2 != null)
+            {
+                ct2.Cancel();
+            }
+            //stop = true;
         }
 
         private void Btn_Stop3_Click(object sender, RoutedEventArgs e)
         {
-            stop = true;
+            if (ct3 != null)
+            {
+                ct3.Cancel();
+            }
+            //stop = true;
+        }
+
+        private void Btn_Stop_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (ct1 != null)
+            {
+                ct1.Cancel();
+            }
+
+             if(ct2!=null)
+            {
+                ct2.Cancel();
+            }
+
+            if(ct3!=null)
+            {
+                ct3.Cancel();
+            }
+
         }
     }
 }
